e226f4c641703a431e16547e4ed482531647d34d
Shwifty0 <ozairmohammad12@gmail.com>
2025-04-20 02:16:54 +0200

    Cleaned up preprocess func to simply use the transform pipline for converting the image to a desired format for the ResNet18 architecture

--- resnet_inference.py
+++ resnet_inference.py
@@ -1,44 +1,41 @@
-import torch
+"""
+This class is for using PyTorch's API.
+The API allows us to use pre-trained models like ResNet-X [X - represents Layers]/
+For the cloud computing project we are using ResNet18 for image classification.
+"""
+
 from torchvision.io import decode_image
 from torchvision.models import resnet18, ResNet18_Weights
+from torchvision.transforms import transforms
 
 
-
+# Global constant variables for the project
 WEIGHTS = ResNet18_Weights.DEFAULT
 MODEL = resnet18(weights=WEIGHTS)
 
 
-class Model_Inference:
+class ModelInference:
     def __init__(self):
-        self.img_path = None
+        self.image = None
         self.weights = WEIGHTS
         self.model = MODEL
     
-    def preprocess_image(self, img_path:str) -> torch.Tensor:
-        # decode uploaded_image
-        self.img_path = img_path
-        self.decoded_image = decode_image(self.img_path)
-
-        # Step 2: Initialize the inference transforms
-        preprocess = self.weights.transforms()
-
-        # Step 3: Apply inference preprocessing transforms
-        batch = preprocess(self.decoded_image).unsqueeze(0)
-        
-        return batch
+    def transform_image(self, image):
+        self.image = image
+        transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor()
+        ])
+        tensor = transform(image).unsqueeze(0)  # Returns FloatTensor in [0, 1]
+        return tensor
 
     def predict(self, image_tensor):
         preprocessed_image = image_tensor
 
         self.model.eval()
-        # Step 4: Use the model and print the predicted category
+        # Use the model and print the predicted category
         prediction = self.model(preprocessed_image).squeeze(0).softmax(0)
         class_id = prediction.argmax().item()
         score = prediction[class_id].item()
         category_name = self.weights.meta["categories"][class_id]
         return f"{category_name}: {100 * score:.1f}%"
-
-
-model = Model_Inference()
-image = model.preprocess_image("/home/shwifty/SOSE25/cloud_computing/ml_serving/n10565667_scuba_diver.JPEG")
-print(model.predict(image))
