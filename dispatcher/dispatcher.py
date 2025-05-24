2e1fa6ba32c9163f5d52364fecc20cce6e1a72e5
Shwifty0 <ozairmohammad12@gmail.com>
2025-05-24 19:50:17 +0200

    starter code and psuedo logic code for the dispatcher

--- dispatcher/dispatcher.py
+++ dispatcher/dispatcher.py
@@ -1,16 +1,23 @@
 import asyncio
 
+
+REQUEST_QUEUE = asyncio.Queue()
+
 class Dispatcher:
-    async def __init__(self):
-        pass
+    def __init__(self, REQUEST_QUEUE):
+        
+        # This queue is going to hold inference requests:
+        self.request_queue = REQUEST_QUEUE
+
     
     
     async def get_requests():
         """
         This function receives requests from the load balancer and puts them in a queue using asyncio.
+        
+        1. Load tester will send 'workload/sec' (workload = number of requests) 
+        2. I need to see how these requests are actually sent and then store them in the asyncio Queue.
         """
-        # This queue is going to hold inference requests:
-        request_queue = asyncio.Queue()
 
         pass
 
